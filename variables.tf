19ffb2531441f37d36e8bc09d51484418a7c19a0
Aayush <aayush@192.168.68.124>
2021-02-06 14:59:21 +0100

    minor name change

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   type    = string
-  default = "us-west-1"
+  default = "eu-west-1"
 }
 
 variable "db_table_name" {
   type    = string
-  default = "terraform-learn"
+  default = "aayush-terraform-learn"
 }
 
 variable "db_read_capacity" {
